fb92fb186c22bae7bf96acfb28dfdc1ae232a8cf
Viktor Johansson <discovicke@gmail.com>
2026-01-21 11:42:29 +0100

    Add useLanguage hook for language and translation management

---
+++ src/i18n/index.ts
@@ -0,0 +1,8 @@
+/**
+ * useLanguage Hook
+ *
+ * Custom hook för att använda språk och översättningar i komponenter.
+ */
+
+export { useLanguage, LanguageProvider } from './LanguageContext';
+export type { Language, TranslationStrings } from './translations';
